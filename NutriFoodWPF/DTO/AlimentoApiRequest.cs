4cabad523c13980160edf1604a41b45c59197f74
May_Croft <mayconpilar19@gmail.com>
2026-05-14 21:32:43 -0300

    Implementação de DTO

---
+++ NutriFoodWPF/DTO/AlimentoApiRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace NutriFoodWPF.DTO
+{
+    class AlimentoApiRequest
+    {
+        public string Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("calories")]
+        public double Calorias { get; set; }
+
+        [JsonPropertyName("serving_size_g")]
+        public double PorcaoGramas { get; set; }
+
+        [JsonPropertyName("fat_total_g")]
+        public double GorduraTotal { get; set; }
+
+        [JsonPropertyName("fat_saturated_g")]
+        public double GorduraSaturada { get; set; }
+
+        [JsonPropertyName("protein_g")]
+        public double Proteina { get; set; }
+
+        [JsonPropertyName("sodium_mg")]
+        public double Sodio { get; set; }
+
+        [JsonPropertyName("potassium_mg")]
+        public double Potassio { get; set; }
+
+        [JsonPropertyName("cholesterol_mg")]
+        public double Colesterol { get; set; }
+
+        [JsonPropertyName("carbohydrates_total_g")]
+        public double Carboidratos { get; set; }
+
+        [JsonPropertyName("fiber_g")]
+        public double Fibras { get; set; }
+
+        [JsonPropertyName("sugar_g")]
+        public double Acucar { get; set; }
+    }
+}
