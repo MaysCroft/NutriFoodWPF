d7deec0f0efdb439b972c2d87553ce348134aabb
May_Croft <mayconpilar19@gmail.com>
2026-05-14 21:52:42 -0300

    Implementação da BaseViewModel

---
+++ NutriFoodWPF/View Models/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace NutriFoodWPF.View_Models
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
